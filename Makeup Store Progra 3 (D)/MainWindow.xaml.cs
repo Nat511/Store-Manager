5df0e6f51ec5e93dcf15d898d0a67da8459021f4
Nataly <Nataly@DESKTOP-R5VUEB1>
2023-06-05 15:00:40 -0400

    Agregar archivos de proyecto.

---
+++ Makeup Store Progra 3 (D)/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Makeup_Store_Progra_3__D_
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private List<Producto> inventario;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            inventario = new List<Producto>();
+        }
+
+        private void AgregarProducto_Click(object sender, RoutedEventArgs e)
+        {
+            string nombre = txtNombre.Text;
+            int cantidad = int.Parse(txtCantidad.Text);
+            DateTime fechaCaducidad = dpFechaCaducidad.SelectedDate ?? DateTime.MinValue;
+
+            Producto producto = new Producto { Nombre = nombre, Cantidad = cantidad, FechaCaducidad = fechaCaducidad };
+            inventario.Add(producto);
+
+            lstProductos.ItemsSource = null;
+            lstProductos.ItemsSource = inventario;
+        }
+    }
+}
---
+++ Makeup Store Progra 3 (D)/Producto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Makeup_Store_Progra_3__D_
+{
+    public class Producto
+    {
+        public string Nombre { get; set; }
+        public int Cantidad { get; set; }
+        public DateTime FechaCaducidad { get; set; }
+    }
+
+}
